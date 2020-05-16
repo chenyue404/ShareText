76fdd90d358bd8cc24d2a274fbe2ad9b16573cb1
chenyue404 <601450225@qq.com>
2020-05-16 17:46:17 +0800

    优化修改端口功能

--- app/src/main/java/com/cy/shareText/SettingsActivity.kt
+++ app/src/main/java/com/cy/shareText/SettingsActivity.kt
@@ -1,11 +1,14 @@
 package com.cy.shareText
 
 import android.content.Intent
-import android.content.SharedPreferences
 import android.os.Bundle
+import android.text.InputType
+import android.text.TextUtils
 import androidx.appcompat.app.AppCompatActivity
+import androidx.preference.EditTextPreference
 import androidx.preference.PreferenceFragmentCompat
 import com.blankj.utilcode.util.ServiceUtils
+import com.blankj.utilcode.util.ToastUtils
 
 class SettingsActivity : AppCompatActivity() {
 
@@ -22,36 +25,35 @@ class SettingsActivity : AppCompatActivity() {
         }
     }
 
-    class SettingsFragment : PreferenceFragmentCompat(),
-        SharedPreferences.OnSharedPreferenceChangeListener {
+    class SettingsFragment : PreferenceFragmentCompat() {
         override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
             setPreferencesFromResource(R.xml.root_preferences, rootKey)
+            val hintStr = getString(R.string.hint_port)
+            val findPreference =
+                findPreference<EditTextPreference>(getString(R.string.preference_port_key))
+            findPreference?.apply {
+                setOnBindEditTextListener {
+                    it.inputType = InputType.TYPE_CLASS_NUMBER
+                    it.hint = hintStr
+                }
+                setOnPreferenceChangeListener { _, newValue ->
+                    val newValueString = newValue.toString()
 
-        }
-
-        override fun onResume() {
-            super.onResume()
-            preferenceManager.sharedPreferences.registerOnSharedPreferenceChangeListener(this)
-        }
-
-        override fun onPause() {
-            super.onPause()
-            preferenceManager.sharedPreferences.unregisterOnSharedPreferenceChangeListener(this)
-        }
-
-        override fun onSharedPreferenceChanged(
-            sharedPreferences: SharedPreferences?,
-            key: String?
-        ) {
-            if (!ServiceUtils.isServiceRunning(WebServer::class.java)) {
-                return
-            }
-            if (key == getString(R.string.preference_port_key)) {
-                activity?.sendBroadcast(
-                    Intent(
-                        activity,
-                        StopServerReceiver::class.java
-                    ).apply { putExtra(StopServerReceiver.NEED_RESTART, true) })
+                    val standard = !TextUtils.isEmpty(newValueString)
+                            && newValueString.toInt() in 1..65535
+                    if (standard) {
+                        if (ServiceUtils.isServiceRunning(WebServer::class.java)) {
+                            activity?.sendBroadcast(
+                                Intent(
+                                    activity,
+                                    StopServerReceiver::class.java
+                                ).apply { putExtra(StopServerReceiver.NEED_RESTART, true) })
+                        }
+                    } else {
+                        ToastUtils.showShort(hintStr)
+                    }
+                    standard
+                }
             }
         }
     }
